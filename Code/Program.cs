9777f3f0f946d621a45669ebd911a98af5609025
Nad <nadl@mail.ru>
2024-07-13 15:48:04 +0300

    Добавлено решение задачи

---
+++ Code/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Code;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] strings = new [] {"Hello", "2", "world", ":-)"};
+        string[] rslt = CreateArray(strings);
+        string joinedResult = string.Join("\",\"", rslt);
+        if(!string.IsNullOrWhiteSpace(joinedResult))
+            joinedResult = $"\"{joinedResult}\"";
+
+        Console.WriteLine($"[{joinedResult}]");
+    }
+
+    private static string[] CreateArray(string[] arrString)
+    {
+        const int maxLength = 3;
+        string[] arrTemp = new string[arrString.Length];
+        int index = -1;
+        foreach(string str in arrString) {
+            if(str.Length > maxLength)
+                continue;
+            arrTemp[++index] = str;
+        }
+        string[] arrNew = new string[index + 1];
+        Array.Copy(arrTemp, 0, arrNew, 0, index + 1);
+
+        return arrNew;
+    }
+}
